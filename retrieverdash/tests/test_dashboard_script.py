4b8af409f62c49db074047c220867c3ceb83d8b7
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-06-05 04:20:46 +0530

    Use pytest-django for test

--- retrieverdash/tests/test_dashboard_script.py
+++ retrieverdash/tests/test_dashboard_script.py
@@ -1,15 +1,16 @@
 import os
 from shutil import rmtree
+from tempfile import mkdtemp
 
-from retriever import datasets
 from retriever.engines import engine_list
+from retriever.lib.load_json import read_json
+from retriever.lib.engine_tools import getmd5
 
 from retrieverdash.dashboard_script.status_dashboard_tools import create_dirs
-from retrieverdash.dashboard_script.status_dashboard_tools import dataset_to_csv
 from retrieverdash.dashboard_script.status_dashboard_tools import diff_generator
-from retrieverdash.dashboard_script.status_dashboard_tools import get_dataset_md5
 
-mysql_engine, postgres_engine, sqlite_engine, msaccess_engine, csv_engine, download_engine, json_engine, xml_engine = engine_list
+mysql_engine, postgres_engine, sqlite_engine, msaccess_engine, \
+csv_engine, download_engine, json_engine, xml_engine = engine_list
 file_location = os.path.dirname(os.path.realpath(__file__))
 precalculated_md5 = '9f6c106f696451732fb763b3632bfd48'
 modified_dataset_url = 'https://raw.githubusercontent.com/weecology/' \
@@ -18,25 +19,46 @@ modified_dataset_url = 'https://raw.githubusercontent.com/weecology/' \
 test_files_location = os.path.join(file_location, 'test_dir')
 
 
+def get_script_module(script_name):
+    """Load a script module."""
+    return read_json(os.path.join(file_location, script_name))
+
+
 def test_status_dashboard():
     if not os.path.exists(os.path.join(file_location, 'test_dir')):
         os.makedirs(os.path.join(file_location, 'test_dir'))
     create_dirs(os.path.join(test_files_location))
     os.chdir(os.path.join(test_files_location, 'old'))
-    dataset = [dataset for dataset in datasets()
-               if dataset.name == 'sample-dataset'][0]
-    dataset_to_csv(dataset)
-    setattr(dataset.tables['main'], 'url', modified_dataset_url)
-    calculated_md5 = get_dataset_md5(dataset,
-                                     location=os.path.join(
-                                         test_files_location,
-                                         'temp_files'))
+    script_module = get_script_module('sample_dataset')
+    sqlite_engine.opts = {'install': 'sqlite', 'file': 'test_db.sqlite3', 'table_name': '{db}_{table}'}
+    sqlite_engine.use_cache = False
+    script_module.download(engine=sqlite_engine)
+    script_module.engine.final_cleanup()
+    script_module.engine.to_csv()
+    os.remove('test_db.sqlite3')
+
+    # Finding the md5 of the modified dataset
+    setattr(script_module.tables['main'], 'url', modified_dataset_url)
+    workdir = mkdtemp()
+    os.chdir(workdir)
+    sqlite_engine.use_cache = False
+    script_module.download(engine=sqlite_engine)
+    script_module.engine.final_cleanup()
+    script_module.engine.to_csv()
+    os.remove('test_db.sqlite3')
+    calculated_md5 = getmd5(os.getcwd(), data_type='dir')
+    rmtree(workdir)
+
+    # If md5 of current dataset doesn't match with current
+    # md5 we have to find the diff
     if calculated_md5 != precalculated_md5:
-        # If md5 of current dataset doesn't match with current
-        # md5 we have to find the diff
         os.chdir(os.path.join(test_files_location, 'current'))
-        dataset_to_csv(dataset)
-        diff_generator(dataset, location=test_files_location)
+        sqlite_engine.use_cache = False
+        script_module.download(sqlite_engine)
+        script_module.engine.final_cleanup()
+        script_module.engine.to_csv()
+        os.remove('test_db.sqlite3')
+        diff_generator(script_module, location=test_files_location)
 
     diff_exist = True if os.path.isfile(
         os.path.join(test_files_location, 'diffs',
