bf56c3e686d17ff3447b2cf3daf0aafbefb9f29e
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-06-07 12:13:24 +0530

    Add dashboard template

--- retrieverdash/core/views.py
+++ retrieverdash/core/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render
+
+import json
 
 
 class DashboardView(View):
+    template_name = "dashboard.html"
+
     def get(self, request, *args, **kwargs):
-        return HttpResponse("This is the dashboard view.")
+        dataset_detail = json.load(open('../dashboard_script/dataset_details.json', 'r'))
+        return render(request, self.template_name, context={'datasets': dataset_detail})
