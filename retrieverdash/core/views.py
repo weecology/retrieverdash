3a2957aef23e001559e1948c36304c1b098accc6
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-06-14 00:35:17 +0530

    Create basic layout with header and table

--- retrieverdash/core/views.py
+++ retrieverdash/core/views.py
@@ -2,12 +2,16 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 
+from retrieverdash.settings.common import PROJECT_ROOT
+
+import os
 import json
 
+file_path = os.path.join(PROJECT_ROOT, 'dashboard_script/dataset_details.json')
 
 class DashboardView(View):
     template_name = "dashboard.html"
 
     def get(self, request, *args, **kwargs):
-        dataset_detail = json.load(open('../dashboard_script/dataset_details.json', 'r'))
+        dataset_detail = json.load(open(file_path, 'r'))
         return render(request, self.template_name, context={'datasets': dataset_detail})
