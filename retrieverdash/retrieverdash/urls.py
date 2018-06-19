b1de90916136f0eada1c99807a4a9b5ab8cd7381
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-06-19 17:43:51 +0530

    Sort dataset names in template

--- retrieverdash/retrieverdash/urls.py
+++ retrieverdash/retrieverdash/urls.py
@@ -1,16 +1,8 @@
-"""retrieverdash URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-"""
-# Django imports
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^blog/', include('blog.urls', namespace='blog')),
 
-    url(r'^admin/', admin.site.urls),
-    url(r'^dashboard/', include('core.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
