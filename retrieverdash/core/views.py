4f623e161a0c14b6b16ec579065704155ffba4b9
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-07-19 14:14:04 +0530

    Improve dashboard view and add a basic template for dashboard (#12)
    Add dashboard template.
    Create basic layout.
    Sort dataset names in template.
    Update Django version to 2.0.0 in requirements.txt.
    Improve design.

--- retrieverdash/core/views.py
+++ retrieverdash/core/views.py
@@ -1,7 +1,27 @@
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render
+
+from retrieverdash.settings.common import PROJECT_ROOT
+
+import os
+import json
+
+file_path = os.path.join(PROJECT_ROOT,
+                         'dashboard_script/dataset_details.json')
+diff_path = os.path.join(PROJECT_ROOT,
+                         'dashboard_script/diffs/')
 
 
 class DashboardView(View):
+    template_name = "dashboard.html"
+
     def get(self, request, *args, **kwargs):
-        return HttpResponse("This is the dashboard view.")
+        dataset_detail = json.load(open(file_path, 'r'))
+        return render(request, self.template_name,
+                      context={'datasets': dataset_detail,
+                               'diff_path': diff_path})
+
+class DiffView(View):
+    def get(self, request, filename):
+        return HttpResponse(open(os.path.join(diff_path, filename)))
