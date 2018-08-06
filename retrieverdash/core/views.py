6b0a6ce9acbbff6e4b10c13bbf1cc2115404aa45
Apoorva Pandey <apoorvapandey365@gmail.com>
2018-08-06 19:01:10 +0530

    Add DashboardView test

--- retrieverdash/core/views.py
+++ retrieverdash/core/views.py
@@ -17,11 +17,14 @@ class DashboardView(View):
     template_name = "dashboard.html"
 
     def get(self, request, *args, **kwargs):
-        json_detail = json.load(open(file_path, 'r'))
+        try:
+            json_detail = json.load(open(file_path, 'r'))
+        except IOError:
+            json_detail = dict({'dataset_details': None, 'last_checked_on': None})
         return render(request, self.template_name,
                       context={'datasets': json_detail['dataset_details'],
-                               'diff_path': diff_path,
-                               'last_checked_on':json_detail['last_checked_on']})
+                               'last_checked_on': json_detail['last_checked_on']})
+
 
 class DiffView(View):
     def get(self, request, filename):
