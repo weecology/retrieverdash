229cd1e0f214cc16cb4c8b99aceddc8a5063c068
Kush Kothari <kushkothari2001@gmail.com>
2021-05-12 15:15:27 +0530

    Added support for spatial datasets

--- retrieverdash/core/urls.py
+++ retrieverdash/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardView, DiffView
+from .views import DashboardView, DiffView, DataSetView
 
 app_name = 'core'
 urlpatterns = [
+    path('dataset/<str:dataset_name>/', DataSetView.as_view(), name='dataset'),
     path('diff/<str:filename>/', DiffView.as_view(), name='diff'),
     path('', DashboardView.as_view(), name='dashboard'),
 ]
